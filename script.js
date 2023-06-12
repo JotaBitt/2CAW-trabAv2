1dc273e6133c045aa3378e60830c733cdcd60788
João Pedro <101153871+JotaBitt@users.noreply.github.com>
2023-06-12 14:23:14 -0300

    petshop incompleto

---
+++ script.js
@@ -0,0 +1,16 @@
+function limpaCampo (num) {
+    if (num == 1) {
+        const campo = document.getElementById("nome");
+        campo.placeholder="";
+    }
+    
+    if (num == 2) {
+        const campo = document.getElementById("email");
+        campo.placeholder="";
+    }
+    if (num == 3) {
+        const campo = document.getElementById("celular");
+        campo.placeholder="";
+    }
+}
+
